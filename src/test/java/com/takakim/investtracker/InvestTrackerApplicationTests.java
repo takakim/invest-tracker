c964695613258175472f6ac95917f81e2317a239
takakim <massanori@gmail.com>
2026-08-21 21:18:26 +0100

    test: add application context smoke test

---
+++ src/test/java/com/takakim/investtracker/InvestTrackerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.takakim.investtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
