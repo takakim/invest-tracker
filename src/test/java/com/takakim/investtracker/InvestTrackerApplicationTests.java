d3b4528de4a867ba0e129b88060ee759ef1340e7
takakim <massanori@gmail.com>
2026-08-21 21:23:01 +0100

    test: update Testcontainers 2 package names

--- src/test/java/com/takakim/investtracker/InvestTrackerApplicationTests.java
+++ src/test/java/com/takakim/investtracker/InvestTrackerApplicationTests.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
